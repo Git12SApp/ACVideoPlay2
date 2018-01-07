c564bdb7f9e77f753b5863cd0675b3f57277dbfb
James Burton <jb76769@gmail.com>
2018-01-07 16:05:13 -0600

    Commit: 4 files as copied from Udemy AC PlayingVideos. 1/7/18

--- ACVideoPlay2/ViewController.swift
+++ ACVideoPlay2/ViewController.swift
@@ -7,12 +7,28 @@
 //
 
 import UIKit
+import AVFoundation
+import AVKit
 
 class ViewController: UIViewController {
+    
+    var playerController = AVPlayerViewController()
+    var player: AVPlayer?
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let videoString: String? = Bundle.main.path(forResource: "SampleVideo_1280x720_5mb", ofType: "mp4")
+        
+        if let url = videoString {
+            
+            let videoURL = NSURL(fileURLWithPath: url)
+            
+            self.player = AVPlayer(url: videoURL as URL)
+            self.playerController.player = self.player
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +36,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func playVideo(_ sender: Any) {
+        
+        self.present(self.playerController, animated: true, completion: {
+            
+            self.playerController.player?.play()
+            
+            })
+    }
+    
 }
 
